d7a63609446f3ab947a963f3c9512a4d87412214
Ram Adhikari <im_ram@hotmail.com>
2022-02-12 12:15:50 +1100

    Updated script

--- files/restore-machinekeys-acl.ps1
+++ files/restore-machinekeys-acl.ps1
@@ -1,5 +1,7 @@
+$t=get-date -format "dd-MM-yyyy-HH-mm-ss"
 $Sddl='O:BAG:BAD:PAI(A;;0x12019f;;;WD)(A;;FA;;;BA)'
 $TargetFolder='C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys'
 $SecDes=get-acl $TargetFolder;
 $SecDes.setsecuritydescriptorsddlform($sddl);
 set-acl -Path $TargetFolder -aclobject $Secdes
+New-Item -Path c:\temp\test_t.txt -Force 
