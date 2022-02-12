c159ef619ccb6df7713a0bd732a0d2633fbda5cf
Ram Adhikari <im_ram@hotmail.com>
2022-02-12 11:36:52 +1100

    added unless for machine key exec

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,11 +13,12 @@ class critical_policy {
     }
   exec { 'machinekayacl':
   command   => file('critical_policy/restore-machinekeys-acl.ps1'),
+  unless    =>'if(((((((get-acl $TargetFolder).access)| select IdentityReference | ft -HideTableHeaders | Out-String).trim()) -replace "\`r\`n", "" ) -replace "              ",",") -ne "Everyone,BUILTIN\Administrators")'
   provider  => powershell,
   }
   exec { 'rename-guest':
   command   => '(Get-WMIObject Win32_UserAccount -Filter "Name=\'guest\'").Rename("new-guest")',
-  unless    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 1 }',
+  onlyif    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 1 }',
   provider  => powershell,
   }
 }
