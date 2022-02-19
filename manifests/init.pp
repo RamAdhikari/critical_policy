ae884d8ea533a1971fd438201e82a6a1fd031ca7
Ram Adhikari <im_ram@hotmail.com>
2022-02-20 10:30:31 +1100

    I hope it will work

--- manifests/init.pp
+++ manifests/init.pp
@@ -53,7 +53,7 @@ registry::value { 'ForceDefenderPassiveMode':
   }
   exec { 'rename-guest':
   command   => '(Get-WMIObject Win32_UserAccount -Filter "Name=\'guest\'").Rename("new-guest")',
-  onlyif    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 1 }',
+  onlyif    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 0 }',
   provider  => powershell,
   }
   service { 'sense':
